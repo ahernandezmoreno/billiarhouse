a393c1c67e7699bf9b73cee3e328341c375b3227
anthonydev9595 <anthonydev9595@gmail.com>
2025-05-06 22:26:00 -0400

    feat:BH-2/Instalando electron y mui, aplicando configuracion basica

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,74 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginImport from "eslint-plugin-import";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default defineConfig([
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    root: true,
+    env: {
+    browser: true,
+    node: true,
+    es2022: true,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module', 
+    },
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      import: pluginImport,
+      'react-hooks': pluginReactHooks,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'eqeqeq': ['error', 'always'],
+      'curly': 'error',
+      'no-implicit-coercion': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'indent': ['error', 2],
+      'comma-dangle': ['error', 'only-multiline'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'import/no-unresolved': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+        },
       ],
+      'import/newline-after-import': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
-  },
-)
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...pluginReact.configs.flat.recommended,
+      'eslint:recommended',
+      'plugin:import/recommended',
+      'plugin:import/errors',
+      'plugin:import/warnings',
+    ],
+  }
+]);
